f8d7c20ab96538f74498fd28a963713391579be0
Patricia de Boer <pdeboer05@gmail.com>
2019-07-15 14:24:04 -0500

    heroku secrets

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,13 @@
 const Sequelize = require('sequelize');
+const databaseName =
+  pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/forecast-app', {
-  logging: false // unless you like the logs
-  // ...and there are many other options you may want to play with
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/forecast-app',
+  {
+    logging: false, // unless you like the logs
+    // ...and there are many other options you may want to play with
+  }
+);
 
 module.exports = db;
